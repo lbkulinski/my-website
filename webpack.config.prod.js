f547a072af0977ba69ea7253a5a2feb531ca98e5
Logan Kulinski <lbkulinski@gmail.com>
2026-07-26 20:31:53 -0500

    Cleanup

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,6 +18,7 @@ module.exports = merge(common, {
         { from: 'styles.css', to: 'styles.css' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
+        { from: 'sitemap.xml', to: 'sitemap.xml' },
         { from: 'icon.png', to: 'icon.png' },
         { from: '404.html', to: '404.html' },
         { from: 'site.webmanifest', to: 'site.webmanifest' },
